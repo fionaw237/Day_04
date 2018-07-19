754052f7436ae4436ba9585075c08f1c7c88b0c5
Fiona Wilson <fionaw237@gmail.com>
2018-07-19 14:19:59 +0100

    Adds debugging files

---
+++ debugging/debugging.rb
@@ -0,0 +1,8 @@
+# def find_item(array, item)
+#   for element in array
+#     if element == item
+#       return item
+#     end
+#   end
+#   return nil
+# end
---
+++ debugging/specs/debugging_spec.rb
@@ -0,0 +1,9 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../debugging.rb")
+
+class DebuggingTest < MiniTest::Test
+
+
+end
